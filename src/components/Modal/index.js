244f07126f9ece6f5ecfee34dd8a7fda922c6c83
jotaEme <78482164+jotaEmeCortat@users.noreply.github.com>
2021-06-30 11:43:19 -0300

    feat(commit): add commit lint

---
+++ src/components/Modal/index.js
@@ -0,0 +1,117 @@
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
+import { motion } from 'framer-motion';
+import { createGlobalStyle } from 'styled-components';
+import { ModalWrapper } from './style/ModalWrapper';
+import { Grid } from '../layout/Grid';
+import { Box } from '../layout/Box';
+import Text from '../commons/Text';
+import { CloseButton } from '../commons/icons';
+
+export const LockScroll = createGlobalStyle`
+  body {
+    overflow: hidden;
+  }
+`;
+
+export default function Modal({
+  // eslint-disable-next-line react/prop-types
+  modal, setModal, onClose, children,
+}) {
+  const modalRef = useRef();
+  const closeModal = ({ target }) => {
+    if (modalRef.current === target) {
+      setModal(!modal);
+    }
+  };
+  return (
+    <ModalWrapper
+      modal={modal}
+    >
+      {modal && <LockScroll />}
+
+      <motion.div
+        variants={{
+          open: {
+            x: 0,
+          },
+          closed: {
+            x: '100%',
+          },
+        }}
+        animate={modal ? 'open' : 'closed'}
+        transition={{
+          duration: 0.5,
+        }}
+        style={{
+          display: 'flex',
+          flex: 1,
+        }}
+      >
+        <ModalWrapper.ModalContent>
+          <Grid.Row
+            flex={1}
+            justifyContent="flex-end"
+            margin={0}
+            ref={modalRef}
+            onClick={closeModal}
+          >
+
+            <Grid.Col
+              display="flex"
+              padding="0"
+              flex={1}
+              value={{
+                xs: 12, md: 6, lg: 5, xl: 4,
+              }}
+            >
+
+              <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="space-around"
+                flex={1}
+                padding={{
+                  xs: '18px',
+                  md: '0 64px',
+                }}
+                background="white"
+                boxShadow="-10px 0px 24px rgba(7, 12, 14, 0.1)"
+              >
+                <ModalWrapper.ModalHeader>
+                  <CloseButton onClick={onClose} />
+                </ModalWrapper.ModalHeader>
+                {children}
+                <ModalWrapper.ModalFooter>
+                  <Text
+                    variant="paragraphy"
+                    tag="p"
+                    color="tertiary.light"
+                    textAlign="center"
+                    padding="16px 0 0 0"
+                  >
+                    {'Já tem uma conta? '}
+                    <Text
+                      href="/login"
+                      variant="paragraphy"
+                      color="secondary.main"
+                    >
+                      Entrar
+                    </Text>
+                  </Text>
+                </ModalWrapper.ModalFooter>
+              </Box>
+            </Grid.Col>
+          </Grid.Row>
+        </ModalWrapper.ModalContent>
+      </motion.div>
+    </ModalWrapper>
+  );
+}
+
+Modal.propTypes = {
+  modal: PropTypes.bool.isRequired,
+  setModal: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
---
+++ src/components/Modal/style/ModalWrapper.js
@@ -0,0 +1,44 @@
+import styled, { css } from 'styled-components';
+
+export const ModalWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  background: rgba(0,0,0,0.5);
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  overflow: scroll;
+  transition: .3s;
+  z-index: 100;
+  ${({ modal }) => {
+    if (modal) {
+      return css`
+        opacity: 1;
+        pointer-events: all;
+      `;
+    }
+    return css`
+      opacity: 0;
+      pointer-events: none;
+    `;
+  }}
+`;
+
+ModalWrapper.ModalContent = styled.div`
+  display: flex;
+  flex:1;
+`;
+
+ModalWrapper.ModalHeader = styled.div`
+  display:flex;
+  /* margin: 0 -16px 0 -16px; */
+`;
+
+ModalWrapper.ModalFooter = styled.div`
+  display:flex;
+  justify-content:center;
+`;
